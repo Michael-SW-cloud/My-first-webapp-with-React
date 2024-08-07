df9c15b421ba1147d7f40b0d3a52f93acfe93782
Michael-SW-cloud <alkhouri@consider-it.de>
2024-08-07 14:43:13 +0200

    Button component

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = ({ label, onClick }) => {
+  return (
+    <div className="div-button">
+      <button variant="contained" onClick={onClick} className="button">
+        {label}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
